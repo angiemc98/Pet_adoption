b38c4afd852512592bd17ee48d8763e68fec6608
angiemc98 <molinanatalia@hotmail.com>
2025-10-17 20:03:14 -0500

    chore: add api routes for pets and applications

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AdoptionApplicationController;
+use App\Http\Controllers\Api\PetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
@@ -24,4 +26,17 @@ Route::post('/login', function (Request $r) {
     }
     $token = $user->createToken('api-token')->plainTextToken;
     return response()->json(['token' => $token, 'user' => $user]);
+
+});
+
+Route::get('/pets', [PetController::class, 'index']);
+Route::get('/pets/{pet}', [PetController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/pets', [PetController::class, 'store']);
+    Route::delete('/pets/{pet}', [PetController::class, 'destroy']);
+    Route::put('/pets/{pet}', [PetController::class, 'update']);
+
+    Route::post('/applications', [AdoptionApplicationController::class, 'store']);
+    Route::patch('/applications/{application}', [AdoptionApplicationController::class, 'update']);
 });
